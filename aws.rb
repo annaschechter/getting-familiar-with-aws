092a66d1fc50f289cb3705c8dce019e58ce6b5c5
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-23 19:00:48 +0000

    Can add aws buckets using the menu

--- aws.rb
+++ aws.rb
@@ -7,13 +7,20 @@ $s3 = AWS::S3.new
 def create_bucket(name)
 	begin
 		$s3.buckets.create(name)
-		resque AWS::S3::Errors::BucketAlreadyExists
+	rescue AWS::S3::Errors::BucketAlreadyExists
 	end
 end
 
 def show_menu
 	puts '(l)ist buckets, (c)reate bucket, (u)pload file, (d)ownload file, (q)uit'
 	response = gets.chomp
+
+	case response
+		when 'c'
+			puts 'bucket name: '
+			name = gets.chomp
+			puts 'error with bucket name, please try again' if !create_bucket(name)
+	end
 end
 
 while show_menu != 'q'; end
